eb668d9676c2ac1d77b3f1d3bf00b92040d53a66
Pankaj <pankajsharma4x@gmail.com>
2025-11-08 21:09:26 +0530

    Updated component X and fixed bug Y

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-indigo-500 to-purple-600 text-white">
+      {/* <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-indigo-500 to-purple-600 text-white"> */}
       <h1 className="text-5xl font-bold mb-4">Tailwind CSS is Working 🚀</h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -14,7 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      {/* </div> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
